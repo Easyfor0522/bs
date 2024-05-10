5448cf48b94fe08bcc7b638d4ff25f448d7d3a83
Easyfor0522 <168514281+Easyfor0522@users.noreply.github.com>
2024-05-10 16:19:58 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -6,7 +6,8 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from dataloaders.ucf_dataset import UCFDataset
-from torchvision.models import resnet101
+import torchvision.models as models
+from torch.autograd import Variable
 
 
 def dataloader(clip_len, batch_size, shuffle, data_type):
@@ -38,12 +39,14 @@ def dataloader(clip_len, batch_size, shuffle, data_type):
     print("start test Dataloader")
     print('train_set_len: ' + str(len(train_dataloader)))
     print('test_set_len: ' + str(len(test_dataloader)))
-    for i_batch, (video, motion, labels) in enumerate(test_dataloader):
-        print('data shape:')
-        print(x.shape)
-        print('labels shape')
-        print(labels.shape)  # 每段视频的 类别
-        break
+    # for i_batch, (video, motion, labels) in enumerate(test_dataloader):
+    #     print('video shape:')
+    #     print(video.shape)
+    #     print('motion shape:')
+    #     print(motion.shape)
+    #     print('labels shape')
+    #     print(labels.shape)  # 每段视频的 类别
+    #     break
 
     
 
@@ -100,11 +103,30 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.shape[0], :]
         return self.dropout(x)
 
-
+def extract_features(resnet, batch):
+    features = []
+    for i in range(batch.size(0)):
+        video = batch[i]
+        video_features = []
+        for j in range(video.size(0)):
+            frame = video[j]
+            #frame = transform(frame)
+            frame = Variable(frame.unsqueeze(0))
+            if torch.cuda.is_available():
+                frame = frame.cuda()
+            feature = resnet(frame)
+            video_features.append(feature.view(feature.size(0), -1))
+        video_features = torch.cat(video_features, dim=0)
+        features.append(video_features)
+    features = torch.stack(features, dim=0)
+    return features
 ########################################################
 # 训练过程, 待完成
 ########################################################
 def train(model, device, train_loader, test_loader, epochs, criterion, optimizer):
+
+    resnet = models.resnet101(pretrained=True)
+    resnet.eval()
 
     model.to(device)
     batches = len(train_loader)
@@ -112,11 +134,14 @@ def train(model, device, train_loader, test_loader, epochs, criterion, optimizer
     for epoch in range(epochs):
         model.train()   # train()模式才能调整网络参数
         batch = -1  # 记录是第几个batch
-        for motion, labels in train_loader:
+        for video, motion, labels in train_loader:
+            feature_video = extract_features(resnet, video)
+            feature_motion = motion.view(64, -1)
+            features = torch.cat((feature_video, feature_motion), dim=1)   # 在维度 1 拼接
             batch += 1
             
             # move to device
-            motion = motion.float().to(device)
+            #motion = motion.float().to(device)
             labels = labels.to(device)
 
             # # print(motion.shape)
@@ -125,7 +150,7 @@ def train(model, device, train_loader, test_loader, epochs, criterion, optimizer
             # motion = resnet(motion)
 
             # forward
-            outputs = model(motion)
+            outputs = model(features)
             loss = criterion(outputs, labels)
 
             # record_loss
@@ -174,31 +199,31 @@ if __name__ == '__main__':
                                            data_type='motion_and_video')
     print('Dataloaders Ready!')     # 如果运行到这里，说明 dataloader OK
 
-    # # 指定设备：cpu or cuda
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # print('device:')
-    # print(device)
-    # # 创建网络模型
-    # input_dim = 33 * 4  # 特征维度：33*4 #
-    # # [batch_size, frames, keypoints, xyzv] -> [batch_size, 时间维度， 特征维度]
-    # hidden_dim = 128     # transformer 隐藏层深度
-    # num_headers = 2     # 多注意力头，建议取值2~8
-    # num_layers = 2      # tf 层数，建议取值2~6
-    # num_classes = 101   # 分类数'''
-    # model = TransformerClassifier(input_dim=input_dim, hidden_dim=hidden_dim,
-    #                               num_headers=num_headers, num_layers=num_layers,
-    #                               num_classes=num_classes)
+    # 指定设备：cpu or cuda
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('device:')
+    print(device)
+    # 创建网络模型
+    input_dim = 2048 + 2112  # 特征维度：33*4 #
+    # [batch_size, frames, keypoints, xyzv] -> [batch_size, 时间维度， 特征维度]
+    hidden_dim = 128     # transformer 隐藏层深度
+    num_headers = 2     # 多注意力头，建议取值2~8
+    num_layers = 2      # tf 层数，建议取值2~6
+    num_classes = 101   # 分类数'''
+    model = TransformerClassifier(input_dim=input_dim, hidden_dim=hidden_dim,
+                                  num_headers=num_headers, num_layers=num_layers,
+                                  num_classes=num_classes)
  
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # model.to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-    # # 设置学习率 和 训练轮数
-    # learning_rate = 0.0001  # 学习率，调整使得 loss逐渐收敛 且 正确率稳定上升
-    # epochs = 1   # 表示遍历epoch遍数据集,测试请设置为 1
+    # 设置学习率 和 训练轮数
+    learning_rate = 0.0001  # 学习率，调整使得 loss逐渐收敛 且 正确率稳定上升
+    epochs = 1   # 表示遍历epoch遍数据集,测试请设置为 1
 
-    # # define Loss and optimizer
-    # criterion = nn.CrossEntropyLoss()   # 交叉熵损失函数，常用于多分类
-    # optimizer = optim.Adam(model.parameters(), lr=learning_rate)    # 优化器不需要改
+    # define Loss and optimizer
+    criterion = nn.CrossEntropyLoss()   # 交叉熵损失函数，常用于多分类
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)    # 优化器不需要改
 
-    # # 开始训练
-    # train(model, device, train_loader, test_loader, epochs, criterion, optimizer)
+    # 开始训练
+    train(model, device, train_loader, test_loader, epochs, criterion, optimizer)
